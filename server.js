e51e9f57dda52cdf9378d095af7725af95a3727b
= <federicohipper@gmail.com>
2020-10-08 16:02:49 -0300

    modifico server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+server.listen(server_port, server_ip_address, function () {
+
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
+
+});
